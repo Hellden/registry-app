bfd19605c9f80ca7450dc01a588a991aaae64ade
Hellden-AIR <r.chapotin@gmail.com>
2023-06-14 17:52:20 +0200

    Update helper

--- lib/helper.cs
+++ lib/helper.cs
@@ -2,6 +2,43 @@ class Helper
 {
   public Helper()
   {
-    Console.WriteLine("Bonjour le Helper");
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine(@"
+              .-""""-.
+             /        \
+            /_        _\
+           // \      / \\
+           |\__\    /__/|
+            \    ||    /
+             \        /
+              \  __  /  \  /          ________________________________
+               '.__.'    \/          /                                 \
+                |  |     /\         |    Dev : Hellden                  |
+                |  |    O  O        |    Project : Registry APP         |
+                ----    //         O \_________________________________/
+               (    )  //        O
+              (\\     //       o
+             (  \\    )      o
+             (   \\   )   /\
+   ___[\______/^^^^^^^\__/) o-)__
+  |\__[=======______//________)__\
+  \|_______________//____________|
+      |||      || //||     |||
+      |||      ||  .||     |||
+       ||      \/  .\/      ||
+                  . .
+                 '.'.`
+");
+    Console.WriteLine(@"
+    Bienvenue sur l'outil de contrôle des serveurs !
+    Un fichier csv est nécessaire pour lister les programmes autorisés.
+    Une colonne Name est requis.
+    ");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("--------------------------------------------------------");
+    Console.WriteLine(@"
+    -h, -help                               aide
+    -g, -generate-csv                       génére un template de csv
+    ");
   }
 }
